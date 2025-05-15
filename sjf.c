3e0cd092050d76e499452fad81f97eaa291c1766
Ajal Sony <ajalsony057@gmail.com>
2025-05-15 23:48:21 +0530

    Add files via upload

---
+++ sjf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+struct process{
+   char name[20];
+int at,tt,bt,wt,status,ct;
+}p[20],temp;
+struct done{
+char name[20];
+int st,ct;
+}d[20];
+void main(){
+    int i,j,n,ls,min,fnd,num,idle;
+    float twt=0.0,ttt=0.0;
+    printf("enter the number of processes:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++);
+    
+}
